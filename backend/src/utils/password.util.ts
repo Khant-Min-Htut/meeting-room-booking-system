cb18095ef123a2a7ab0325b5386daf17e6b45612
khantMinHtut <khantminhtut009@gmail.com>
2026-07-20 00:02:56 +0700

    Change Prisma datasource to PostgreSQL

--- backend/src/utils/password.util.ts
+++ backend/src/utils/password.util.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
 const SALT_ROUNDS = 10;
 
@@ -6,6 +6,9 @@ export const hashPassword = async (password: string): Promise<string> => {
   return bcrypt.hash(password, SALT_ROUNDS);
 };
 
-export const comparePassword = async (password: string, hash: string): Promise<boolean> => {
+export const comparePassword = async (
+  password: string,
+  hash: string,
+): Promise<boolean> => {
   return bcrypt.compare(password, hash);
 };
